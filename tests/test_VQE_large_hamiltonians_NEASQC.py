4fd4306b163e362c9b1b9d8b7ad7aaf483ed7047
wsennane <43382236+wsennane@users.noreply.github.com>
2024-09-26 16:14:35 +0200

    Add files via upload

---
+++ tests/test_VQE_large_hamiltonians_NEASQC.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pickle
+
+
+from Github_calc_Hamilt import graphene_co2_dist
+from Github_calc_Energy import save_E_into_dict
+
+
+#############################################################
+################# Uses VQE on Graphene+CO2. #################
+#############################################################
+
+nx, ny = 4,1
+basis_set = 'sto-3g'
+
+hamilt_filename = f'graphene_co2_{nx}_{ny}_{basis_set}_LARGE.H.pickle'
+save_filename = f'graphene_co2_{nx}_{ny}_{basis_set}_LARGE'
+
+l_nl = [4,5,6]
+l_nh = [4,5,6]
+
+l_d_graph_co2 = [3.0]                            ## Test
+
+print(f'\n########## MOLECULE : Graphene ({nx},{ny}) + CO2  ##########\n')    
+
+for nb_homo, nb_lumo in zip(l_nh, l_nl):
+    print(f'\n----- {nb_homo} HOMO - {nb_lumo} LUMO -----')
+    
+    for i_co2, d_graph_co2 in enumerate(l_d_graph_co2):
+        print(f'- - - - {i_co2+1}/{len(l_d_graph_co2)} - - - -')
+
+        ## HOLLOW approach (cf. https://pubs.acs.org/doi/10.1021/acsomega.3c03251)
+        mol, m_mol = graphene_co2_dist(d_graph_co2=d_graph_co2, alpha=1, 
+                                       nx=nx, ny=ny, 
+                                       dm0=None,
+                                       th_ext_H=30, th_ext_H2=-5,
+                                       th_co2 = 90, phi_co2 = 0,
+                                       basis_set=basis_set)
+
+        ## qUCC calculation 
+        dic_E_save = save_E_into_dict(d_graph_co2, hamilt_filename, save_filename, 
+                                      mol, m_mol, nb_homo, nb_lumo, 
+                                      ansatz="qUCC", nbshots=0, N_trials=1)
+
+
+
---
+++ tests/test_build_large_hamiltonians_NEASQC.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pickle
+
+from Github_calc_Hamilt import save_H_into_dict, graphene_co2_dist
+    
+    
+############################################################
+##### Prepares the Hamiltonians for Graphene+CO2 calc. #####
+############################################################
+
+nx, ny = 4,1
+basis_set = 'sto-3g'
+
+hamilt_filename = f'graphene_co2_{nx}_{ny}_{basis_set}_LARGE'
+save_filename = f'graphene_co2_{nx}_{ny}_{basis_set}_LARGE'
+
+l_nl = [4,5,6]
+l_nh = [4,5,6]
+
+l_d_graph_co2 = [3.0]                            ## Test
+
+print(f'\n########## MOLECULE : Graphene ({nx},{ny}) + CO2  ##########\n')    
+
+for nb_homo, nb_lumo in zip(l_nh, l_nl):
+    print(f'\n----- {nb_homo} HOMO - {nb_lumo} LUMO -----')
+    
+    if 2*(nb_homo+nb_lumo) > 16:                   ## 02/09/2024
+        break
+    
+    for i_co2, d_graph_co2 in enumerate(l_d_graph_co2):
+        print(f'- - - - {i_co2+1}/{len(l_d_graph_co2)} - - - -')
+        
+        ## HOLLOW approach (cf. https://pubs.acs.org/doi/10.1021/acsomega.3c03251)
+        mol, m_mol = graphene_co2_dist(d_graph_co2=d_graph_co2, alpha=1, 
+                                       nx=nx, ny=ny, 
+                                       dm0=None,
+                                       th_ext_H=30, th_ext_H2=-5,
+                                       th_co2 = 90, phi_co2 = 0,
+                                       basis_set=basis_set)
+
+        dic_H_save = save_H_into_dict(d_graph_co2, save_filename, mol, m_mol, nb_homo, nb_lumo, calc_E_exact=False)
+
